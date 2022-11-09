97e10b5acfc7d4808ee7ce5187da96694f9181c9
Carlos Vasquez <carvasq@ucdavis.edu>
2022-11-09 15:57:17 -0800

    added all CRUD operations to Drivers

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, json, redirect
+from flask import Flask, render_template, json, redirect, request
 from flask_mysqldb import MySQL
 from flask import request
 import os
+import database.db_connector as db
 
+db_connection = db.connect_to_database()
 app = Flask(__name__)
+
+
 
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
 app.config['MYSQL_USER'] = 'cs340_vasqucar'
@@ -31,10 +35,70 @@ def cars():
     return render_template('cars.j2')
 
 
-@app.route('/Drivers')
+@app.route('/Drivers', methods=["POST", "GET"])
 def drivers():
-    return render_template('drivers.j2')
+    if request.method == "POST":
+        if request.form.get("add_driver"):
+            first_name_input = request.form["first_name"]
+            last_name_input = request.form["last_name"]
+            email_input = request.form["email"]
+            phone_input = request.form["phone"]
+            street_input = request.form["street"]
+            city_input = request.form["city"]
+            state_input = request.form["state"]
+            zipcode_input = request.form["zipcode"]
+            query_parameters = [first_name_input,last_name_input,email_input,phone_input,street_input,city_input,state_input,zipcode_input]
 
+            query = "INSERT INTO Drivers (first_name, last_name, email, phone, street, city, state, zipcode) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
+            cursor = db.execute_query(db_connection=db_connection, query=query, query_params=query_parameters)
+            return redirect("/Drivers")
+
+        elif request.form.get("search_driver"):
+            last_name_input = request.form["last_name"]
+            query = "SELECT * FROM Drivers WHERE last_name LIKE %s;"
+            cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(last_name_input + '%',))
+            results = cursor.fetchall()
+            return render_template('drivers.j2', Drivers=results)
+        
+    else:
+        query = "SELECT * FROM Drivers;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+        return render_template('drivers.j2', Drivers=results)
+
+
+@app.route('/Drivers/update/<int:id>', methods=["POST", "GET"])
+def drivers_update(id):
+    if request.method == "POST":
+        if request.form.get("update_driver"):
+            first_name_input = request.form["first_name"]
+            last_name_input = request.form["last_name"]
+            email_input = request.form["email"]
+            phone_input = request.form["phone"]
+            street_input = request.form["street"]
+            city_input = request.form["city"]
+            state_input = request.form["state"]
+            zipcode_input = request.form["zipcode"]
+            query_parameters = [first_name_input,last_name_input,email_input,phone_input,street_input,city_input,state_input,zipcode_input]
+            query_parameters.append(id)
+
+            query = "UPDATE Drivers SET Drivers.first_name = %s, Drivers.last_name = %s, Drivers.email = %s, Drivers.phone = %s, Drivers.street = %s, Drivers.city = %s, Drivers.state = %s, Drivers.zipcode = %s WHERE Drivers.driver_id = %s"
+            cursor = db.execute_query(db_connection=db_connection, query=query, query_params=query_parameters)
+            
+        return redirect("/Drivers")
+    else:
+        query = "SELECT * FROM Drivers WHERE driver_id = %s;"
+        cursor = db.execute_query(db_connection=db_connection, query=query, query_params=[id])
+        results = cursor.fetchall()
+        return render_template('drivers_update.j2', driver=results[0])
+
+
+@app.route('/Drivers/delete/<int:id>')
+def drivers_delete(id):
+    query = "DELETE FROM Drivers WHERE driver_id = %s;"
+    cursor = db.execute_query(db_connection=db_connection, query=query, query_params=[id])
+    return redirect("/Drivers")
+    
 
 @app.route('/Drivers_Rentals')
 def drivers_rentals():
